076442bf62ab420e8a032c948481e2158121aa6c
msla1219 <76463854+msla1219@users.noreply.github.com>
2022-06-02 07:23:19 +0900

    Add files via upload

--- linked_list.py
+++ linked_list.py
@@ -1,27 +1,32 @@
 import hashlib
 
+
 class Block:
     def __init__(self, index, timestamp, content, previous_hash):
-      self.index = index
-      self.timestamp = timestamp
-      self.content = content
-      self.previous_hash = previous_hash
-      self.hash = self.calc_hash()
-   
+        self.index = index
+        self.timestamp = timestamp
+        self.content = content
+        self.previous_hash = previous_hash
+        self.hash = self.calc_hash()
+
     def calc_hash(self):
-      sha = hashlib.sha256()
-      sha.update(str(self.index).encode('utf-8') + 
-                 str(self.timestamp).encode('utf-8') + 
-                 str(self.content).encode('utf-8') + 
-                 str(self.previous_hash).encode('utf-8'))
-      return sha.hexdigest()
-      
+        sha = hashlib.sha256()
+        sha.update(str(self.index).encode('utf-8') +
+                   str(self.timestamp).encode('utf-8') +
+                   str(self.content).encode('utf-8') +
+                   str(self.previous_hash).encode('utf-8'))
+        return sha.hexdigest()
+
+
 M4BlockChain = []
 
 from datetime import datetime
+
+
 def create_genesis_block():
     return Block(0, datetime.now(), "Genesis Block", "0")
-    
+
+
 M4BlockChain.append(create_genesis_block())
 
 
@@ -41,8 +46,7 @@ def app_five(block_list):
 
     for i in range(5):
         M4BlockChain.append(
-                    Block(index + i,
+                    Block(index+i,
                     datetime.now(),
-                    "this is block " + str(index + i),
+                    "this is block " + str(index+i),
                     block_list[index - 1].hash))
-
